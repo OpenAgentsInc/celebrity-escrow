149f346dd20f9fe4d1984bf0a97f5cfa8f6aa223
erik aronesty <erik@q32.com>
2023-05-17 11:48:11 -0400

    resolver

--- nostrescrow/rollup.config.js
+++ nostrescrow/rollup.config.js
@@ -1,20 +1,26 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
 
 export default [
 	// browser-friendly UMD build
 	{
 		input: 'src/index.ts',
 		output: {
+            interop: "compat",
             sourcemap: true,
 			format: 'umd',
             dir: 'lib',
             name: "NostrEscrow",
 		},
 		plugins: [
-			resolve(),
 			commonjs(),
+			resolve({
+                preferBuiltins: true,
+                browser: true
+            }),
+			json(),
 			typescript() 
 		]
 	},
